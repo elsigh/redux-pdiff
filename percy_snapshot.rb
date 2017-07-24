6a61a2c3c74d8395401d0eb78e044da07ed92c15
Lindsey Simon <lindsey@quip.com>
2017-07-24 16:08:24 -0700

    keep tokenz out of repo

--- percy_snapshot.rb
+++ percy_snapshot.rb
@@ -1,7 +1,5 @@
 require 'percy/capybara/anywhere'
 ENV['PERCY_DEBUG'] = '1'  # Enable debugging output.
-ENV['PERCY_TOKEN'] = '89e910b3c5916accb683e5a524fe0ac9b48adbdfb4ac7b64d526c241f9b40461'
-ENV['PERCY_PROJECT'] = 'Commoner/Redux-PDiff'
 
 # Configuration.
 server = 'http://localhost:5000'
