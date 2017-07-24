6a897677163f685c11c745c1bad7c888a6de1d7a
Lindsey Simon <lindsey@quip.com>
2017-07-24 16:14:32 -0700

    changes strikethrough color

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import App from './containers/App'
 import reducer from './reducers'
 import 'todomvc-app-css/index.css'
+import './index.css'
 
 const store = createStore(reducer)
 
